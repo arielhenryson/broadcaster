30c02298cd400a7ba205e2a59a3c3a7b4a6ca67e
ariel.henryson <ariel.henryson@gmail.com>
2022-11-24 13:48:21 +0200

    work

--- broadcaster/src/main.rs
+++ broadcaster/src/main.rs
@@ -1,7 +1,11 @@
-use rocket::State;
-use rocket::serde::{Serialize, Deserialize};
-use rocket::tokio::sync::broadcast::{channel, Sender};
+use std::{thread, time::Duration};
+use rocket::fs::{FileServer,relative};
+use rocket::{State, Shutdown};
 use rocket::form::Form;
+use rocket::response::stream::{EventStream, Event};
+use rocket::serde::{Serialize, Deserialize};
+use rocket::tokio::sync::broadcast::{channel, Sender, error::RecvError};
+use rocket::tokio::select;
 
 #[macro_use] extern crate rocket;
 
@@ -16,16 +20,37 @@ pub struct Message {
     pub message: String
 }
 
-#[get("/")]
-fn index() -> &'static str {
-    "brodcaster is running"
-}
-
 #[post("/message", data = "<form>")]
 fn post(form: Form<Message>, queue: &State<Sender<Message>>) -> &'static str {
+    let val = form.into_inner().clone();
 
-    let _res = queue.send(form.into_inner());
+    for _n in 0..10000 {
+        let val2 = val.clone();
+
+        thread::sleep(Duration::from_millis(100));
+        let _res = queue.send(val2);
+    }
     "brodcaster is posting"
+}
+
+#[get("/events")]
+async fn events(queue: &State<Sender<Message>>, mut end: Shutdown) -> EventStream![] {
+    let mut rx = queue.subscribe();
+
+    EventStream! {
+        loop {
+            let msg = select! {
+                msg = rx.recv() => match msg {
+                    Ok(msg) => msg,
+                    Err(RecvError::Closed) => break,
+                    Err(RecvError::Lagged(_)) => continue,
+                },
+                _ = &mut end => break,
+            };
+
+            yield Event::json(&msg);
+        }
+    }
 }
 
 #[launch]
@@ -33,7 +58,8 @@ fn rocket() -> _ {
     rocket::build()
         .manage(channel::<Message>(1024). 0)
         .mount("/", routes![
-            index,
-            post
+            post,
+            events
         ])
+        .mount("/", FileServer::from(relative!("static")))
 }
